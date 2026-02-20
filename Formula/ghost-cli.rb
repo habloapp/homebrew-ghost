805cbfd4f83ea663869c5f239f45a1502644db2a
Ghost Networks <noreply@gh-o.net>
2026-02-20 19:54:49 -0300

    ghost-cli 0.30.0

--- Formula/ghost-cli.rb
+++ Formula/ghost-cli.rb
@@ -1,23 +1,23 @@
 class GhostCli < Formula
   desc "CLI for Ghost Networks — manage networks, devices, and WireGuard tunnels"
   homepage "https://gh-o.net"
-  version "0.29.0"
+  version "0.30.0"
   license "Proprietary"
 
   on_macos do
     url "https://ghost-pull-zone.b-cdn.net/d/cli/#{version}/macos/ghost-cli.tar.gz"
-    sha256 "728908cd162790015223d2ed08a71d77f81d4fe56a415384b24d6cc956980d80"
+    sha256 "4970edb127f5e1e2641f259267d80301c01da9b8689b0bca22e23bbf6e67f05b"
   end
 
   on_linux do
     on_intel do
       url "https://ghost-pull-zone.b-cdn.net/d/cli/#{version}/linux-x64/ghost-cli.tar.gz"
-      sha256 "803da49ece43cb7bd768fac5c60fe75f00e4c31b9eb575b6e74cf3aa8edbdcde"
+      sha256 "253cab265187cc987b807b38211ad98d6bc56ac8b03fd6a1ebca3061cd6fcfb6"
     end
 
     on_arm do
       url "https://ghost-pull-zone.b-cdn.net/d/cli/#{version}/linux-arm64/ghost-cli.tar.gz"
-      sha256 "5d07db23745227c2683cdaf5a3e5422d03eb325f8216c2c702c4ce4c857845df"
+      sha256 "22e34e69f3d4a1190a977ab0ea33b8a5fcd8d4057eb4201d8655c2d549b6759e"
     end
   end
 
