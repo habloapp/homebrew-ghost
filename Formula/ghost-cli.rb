aac88457de60781a15267b07a0b284612ce32288
Ghost Networks <noreply@gh-o.net>
2026-02-25 19:45:34 -0300

    ghost-cli 0.36.0

--- Formula/ghost-cli.rb
+++ Formula/ghost-cli.rb
@@ -6,18 +6,18 @@ class GhostCli < Formula
 
   on_macos do
     url "https://ghost-pull-zone.b-cdn.net/d/cli/#{version}/macos/ghost-cli.tar.gz"
-    sha256 "a4a69c620356caa4a257819a2ffb8b69ab354e1866f9e41f2e9b83753ec50dfb"
+    sha256 "62fda05c4fd9e38f25fccac63d862632f4b32f0acff88fce085f224c63fceca0"
   end
 
   on_linux do
     on_intel do
       url "https://ghost-pull-zone.b-cdn.net/d/cli/#{version}/linux-x64/ghost-cli.tar.gz"
-      sha256 "d72ae600784527fa929dbacfb8da04e90f26da052b237a7cd3f99b5ad02b733b"
+      sha256 "384fbcb4b53dd970563f25c03b0e9fc8a1373c6e973d653eff7125e376abf40d"
     end
 
     on_arm do
       url "https://ghost-pull-zone.b-cdn.net/d/cli/#{version}/linux-arm64/ghost-cli.tar.gz"
-      sha256 "6f6a8aeb052918bca552103a41ab160493bbf2d81795cf8597d2be5b8ffa47f1"
+      sha256 "6314a55e43fccc485197f9a2d891f2a7a5ab6fc28cebec1e5139901cdd1e14a3"
     end
   end
 
