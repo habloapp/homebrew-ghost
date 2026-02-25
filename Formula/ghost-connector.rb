7289773a35f59cb751cbedf816c26764dd926924
Ghost Networks <noreply@gh-o.net>
2026-02-25 19:43:36 -0300

    ghost-connector 0.36.0

--- Formula/ghost-connector.rb
+++ Formula/ghost-connector.rb
@@ -1,23 +1,23 @@
 class GhostConnector < Formula
   desc "Ghost Networks Connector — always-on WireGuard tunnel agent for any device"
   homepage "https://gh-o.net"
-  version "0.35.0"
+  version "0.36.0"
   license "Proprietary"
 
   on_macos do
     url "https://ghost-pull-zone.b-cdn.net/d/connector/#{version}/macos-arm64/ghost-connector.tar.gz"
-    sha256 "4a091615898aef37561ffa91ea9e2e9f3ed58b8d95597dc7424c3feca0cdc037"
+    sha256 "4f8f9480a8a36b7e172b079441382a2fe4947b86b0c663ab5e672b6fb3b57578"
   end
 
   on_linux do
     on_intel do
       url "https://ghost-pull-zone.b-cdn.net/d/connector/#{version}/linux-x64/ghost-connector.tar.gz"
-      sha256 "608a495f4bacad36b0fd45a47ca794a992b2b285b182f4a0ead36bc202bc9b74"
+      sha256 "a48d077daa341ed4fc6ac9d5587ccad8676ac37509ebd60168b218c1446161d4"
     end
 
     on_arm do
       url "https://ghost-pull-zone.b-cdn.net/d/connector/#{version}/linux-arm64/ghost-connector.tar.gz"
-      sha256 "cfb884c09f7a7209f8de6e0cb7ab4c9ed63c59037fce82d33ec1b231e383bbe2"
+      sha256 "309f11fbaef6b57d4702693516ce2ad82279dcb10136e23cd62caebd9e913e25"
     end
   end
 
